970b313f5ff74257eb8f2b30ae4c40c12fb63728
Gokulram V <goku2645@global.chainsys.com>
2021-05-26 15:28:28 +0530

    Diaplay language feature with Json

--- src/main/java/in/gokul/util/ConnectionUtil.java
+++ src/main/java/in/gokul/util/ConnectionUtil.java
@@ -3,6 +3,7 @@ package in.gokul.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import in.gokul.exception.DbException;
@@ -11,18 +12,25 @@ public class ConnectionUtil {
 	private ConnectionUtil() {
 
 	}
-
-	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-		String driverClass = "org.postgresql.Driver";
-		String url = "jdbc:postgresql://localhost/Musify";
-		String username = "postgres";
-		String password = "Gokulram@26";
-		Class.forName(driverClass);
-		Connection connection = DriverManager.getConnection(url, username, password);
-		System.out.println("Connection Created");
-		return connection;
+   /**
+    * this method gets the connection to the database using system variables
+    * @return
+    */
+	public static Connection getConnection() {
+		try {
+			Class.forName(System.getenv("spring.datasource.driver-class-name"));
+			return DriverManager.getConnection(System.getenv("spring.datasource.url"),
+					System.getenv("spring.datasource.username"), System.getenv("spring.datasource.password"));
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+			throw new DbException("Can't establish connection");
+		}
 	}
-
+   /**
+    * This method closes the connection and prepared statement 
+    * @param pst
+    * @param con
+    */
 	public static void close(PreparedStatement pst, Connection con) {
 		try {
 			if (con != null) {
@@ -38,4 +46,31 @@ public class ConnectionUtil {
 			throw new DbException("INVALID CONNECTION");
 		}
 	}
+	/**
+	 * This method closes the resultSet, preparedStatement and connection
+	 * @param res
+	 * @param pst
+	 * @param con
+	 */
+	public static void close(ResultSet res,PreparedStatement pst, Connection con) {
+		try {
+			if (con != null) {
+				con.close();
+
+			}
+			if (pst != null) {
+				pst.close();
+
+			}
+			if(res!=null)
+			{
+				res.close();
+			}
+
+		} catch (SQLException e) {
+			throw new DbException("INVALID CONNECTION");
+		}
+	}
+	
+	
 }
